ecbb7fee9451dc4dcbcd152e26a7aa1c38952139
Ben Z <bzukin2025@kehillahstudent.org>
2023-05-18 10:45:31 -0700

    world pop graph bar

--- final_project.py
+++ final_project.py
@@ -1,28 +1,23 @@
-import requests # this pulls data out of websites
-import bs4  #this allows to call the data in differnet ways 
-import pandas as pd  # turns link into html  
-import matplotlib.pyplot as plt  #plots data
+import pandas as pd #This import pulls the data from the links and allows it to be graphed.  
+import matplotlib.pyplot as plt #This import graphs the data. 
 
-#the url for the world population 
+#This is the link from where the information comes from  
 url = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
-#reads the html into pandas 
+#This function reads the html link and makes it usable for pandas so data can be taken.  
 dfs = pd.read_html(url)
- #finds the correct data frame to take the data to plot from  
+#This defines the data frame as the correct data from which I need source the information from.  
 df = dfs[1]
 
-#print(df[('Rank', 'Rank')])
-
-
-#defines df as the data country and population
+#This defines the Data I want to pull as the Country and Population.  
 df = df[[('Country / Dependency', 'Country / Dependency'), ('Population', 'Numbers')]]
-#makes the data of populaitons into an integer 
+#This turns the data of the "Populaitons" into an integer which allows it to be on the y-axis. 
 df = df.astype({ ('Population', 'Numbers'):'int'})
-#df = df. head(10)
-#picks rows 1 to 11
+#This selects rows 1 to 11 of the data.
 df = df.loc[1:11]
 
-#plots the grpah with the country on the x axis ands populaiton opn the y axis. It also makes the grpah a bar graph
+#This plots the data of the "Country" on the x=axis and the "Populaiton" on the y-axis.
+#It also makes the grpah a bar graph.
 df.plot(x=('Country / Dependency','Country / Dependency'), y= ('Population', 'Numbers'), kind = "bar")
-#shows the graph
+#This shows the graph once it is complete. 
 plt.show()
 
